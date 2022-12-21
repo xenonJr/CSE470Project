911dc7c02156f7f1dbabafc6fc56a6de68c16f0a
zkxenon <zkxenon98@gmail.com>
2022-12-21 21:02:58 +0600

    added unit test

--- app/src/test/java/com/example/pd/ExampleUnitTest.java
+++ app/src/test/java/com/example/pd/ExampleUnitTest.java
@@ -1,8 +1,11 @@
 package com.example.pd;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+
+import com.example.pd.Model.Doner;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +13,52 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
-    @Test
+    @Ignore
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
+
+    @Test
+    public void getJela(){
+        String expected = "Dhaka";
+        Doner doner = new Doner("Xenon", "Dhaka", "o+", "Tangail", "Male", 23);
+        String actual = doner.getJela();
+        assertEquals(expected,actual);
+    }
+
+
+    @Test
+    public void getName(){
+        String expected = "Xenon";
+        Doner doner = new Doner("Xenon", "Dhaka", "o+", "Tangail", "Male", 23);
+        String actual = doner.getName();
+        assertEquals(expected,actual);
+    }
+
+
+    @Test
+    public void getBg(){
+        String expected = "o+";
+        Doner doner = new Doner("Xenon", "Dhaka", "o+", "Tangail", "Male", 23);
+        String actual = doner.getBg();
+        assertEquals(expected,actual);
+    }
+
+
+    @Test
+    public void getUpojela(){
+        String expected = "Tangail";
+        Doner doner = new Doner("Xenon", "Dhaka", "o+", "Tangail", "Male", 23);
+        String actual = doner.getUpojela();
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void getNumber(){
+        int expected = 23;
+        Doner doner = new Doner("Xenon", "Dhaka", "o+", "Tangail", "Male", 23);
+        int actual = doner.getNumber();
+        assertEquals(expected,actual);
+    }
+
 }
